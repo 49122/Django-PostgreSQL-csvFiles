1cebd95872f270fdda74f4edc248fbf5f3387b77
Jorge <jorgealbertodelgadilloalonso@gmail.com>
2021-04-23 17:22:28 -0500

    Adde elements to tables and learn how to use Points

--- CSVPROYECTO/csvapp/models.py
+++ CSVPROYECTO/csvapp/models.py
@@ -1,10 +1,14 @@
 from django.contrib.gis.db import models
 from django.contrib.gis.geos import Point
+from django.utils import timezone
 # Create your models here.
 
 class Dataset(models.Model):
     name = models.CharField(max_length=95)
-    date = models.DateField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 class Row(models.Model):
     dataset_id = models.ForeignKey(Dataset, on_delete=models.CASCADE)
--- CSVPROYECTO/csvapp/views.py
+++ CSVPROYECTO/csvapp/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse
+import csv
 
 # Create your views here.
 
+def addcsv(request):
+    return HttpResponse("BUUUUE .")
